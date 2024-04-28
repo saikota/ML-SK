220c67d99f057f0f60375c369d6907cba74671a1
SaiKota <sai.kota@hotmail.com>
2024-04-28 06:37:24 +0000

    v1- addition, substraction, division of vectors

---
+++ LinearAlgebra-1/class-colab-prg/class.py
@@ -0,0 +1,26 @@
+import numpy as np
+# myList = [1, 2, 3]
+# myVector = np.array(myList)
+
+# print(myVector)
+
+
+# Addition
+# vec1 = np.array([10,20,30,40,50])
+# vec2 = np.array([1,2,3,4,5])
+# sum = vec1+vec2
+# print(sum)
+
+
+# Subtraction
+# vec1 = np.array([10,20,30,40,50])
+# vec2 = np.array([1,2,3,4,5])
+# sub = vec1-vec2
+# print(sub)
+
+
+# Division
+vec1 = np.array([10,20,30,40,50])
+vec2 = np.array([1,4,4,4,5])
+div = vec1/vec2
+print(div)
